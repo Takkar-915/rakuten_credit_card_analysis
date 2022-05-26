422b2c6a0c2250b36bda83ccfcf386b53ebc4640
takkar <yurisuki915@gmail.com>
2022-05-26 23:55:31 +0900

    secret_keyを変更し、gitで追跡できないようにした

--- credit_card/settings.py
+++ credit_card/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lo6qqdfr@@cy3sk^!=g2001t245sp+2$jkcr3!)&wl!24=6%5%'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #デプロイ時はFalseにする！
@@ -125,3 +124,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
